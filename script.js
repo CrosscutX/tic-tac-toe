370337d7d56f20f1a3ad523ff8311b1669aaa428
Crosscut <crosscutcode@gmail.com>
2022-08-13 16:24:42 -0400

    Logic handling player wins done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const player = (name) =>{
-   const congrats = () => `${name} has won!`
-    return{congrats};
+   const congrats = () => name + " has won!";
+    return {name, congrats};
 }
 
-//const player1 = player(prompt('Enter the name of player 1.'));
-//const player2 = player(prompt('Enter the name of player 2.'));
+const player1 = player(prompt('Enter the name of player 1.'));
+const player2 = player(prompt('Enter the name of player 2.'));
 
 //Event listeners for board
 let space1 = document.querySelector('.space-1');
@@ -16,15 +16,79 @@ let space6 = document.querySelector('.space-6');
 let space7 = document.querySelector('.space-7');
 let space8 = document.querySelector('.space-8');
 let space9 = document.querySelector('.space-9');
+let title = document.querySelector('h1');
 //turn 0 is player 1, turn 1 is player 2, gameboard primed for input
 const gameFlow = (() =>{
     let turn = 0;
-    let gameBoardArr = [null,null,null,null,null,null,null,null,null];
+    let gameBoardArr = ['','','','','','','','',''];
+   
     return{
         turn,
         gameBoardArr,
     };
 })();
+ //check to see who has won the game
+
+function checkWin(){
+  
+    gameFlow.gameBoardArr.forEach(element =>{
+    //player 1 victory horizontal
+        if(gameFlow.gameBoardArr[0] == 'X' && gameFlow.gameBoardArr[1] =='X' && gameFlow.gameBoardArr[2] == 'X'){
+            title.innerText = player1.congrats();
+        }else if(gameFlow.gameBoardArr[3] == 'X' && gameFlow.gameBoardArr[4] =='X' && gameFlow.gameBoardArr[5] == 'X'){
+            title.innerText = player1.congrats();
+        }else if(gameFlow.gameBoardArr[6] == 'X' && gameFlow.gameBoardArr[7] =='X' && gameFlow.gameBoardArr[8] == 'X'){
+            title.innerText = player1.congrats();
+        }
+
+        //player 1 victory vertical
+        if(gameFlow.gameBoardArr[0] == 'X' && gameFlow.gameBoardArr[3] =='X' && gameFlow.gameBoardArr[6] == 'X'){
+            title.innerText = player1.congrats();
+        }else if(gameFlow.gameBoardArr[1] == 'X' && gameFlow.gameBoardArr[4] =='X' && gameFlow.gameBoardArr[7] == 'X'){
+            title.innerText = player1.congrats();
+        }else if(gameFlow.gameBoardArr[2] == 'X' && gameFlow.gameBoardArr[5] =='X' && gameFlow.gameBoardArr[8] == 'X'){
+            title.innerText = player1.congrats();
+        }
+
+        //player 1 diagonal victory
+        if(gameFlow.gameBoardArr[0] == 'X' && gameFlow.gameBoardArr[4] =='X' && gameFlow.gameBoardArr[8] == 'X'){
+            title.innerText = player1.congrats();
+        }else if(gameFlow.gameBoardArr[6] == 'X' && gameFlow.gameBoardArr[4] =='X' && gameFlow.gameBoardArr[2] == 'X'){
+            title.innerText = player1.congrats();
+        }
+
+         //player 2 victory horizontal
+         if(gameFlow.gameBoardArr[0] == 'O' && gameFlow.gameBoardArr[1] =='O' && gameFlow.gameBoardArr[2] == 'O'){
+            title.innerText = player2.congrats();
+        }else if(gameFlow.gameBoardArr[3] == 'O' && gameFlow.gameBoardArr[4] =='O' && gameFlow.gameBoardArr[5] == 'O'){
+            title.innerText = player2.congrats();
+        }else if(gameFlow.gameBoardArr[6] == 'O' && gameFlow.gameBoardArr[7] =='O' && gameFlow.gameBoardArr[8] == 'O'){
+            title.innerText = player2.congrats();
+        }
+
+        //player 2 victory vertical
+        if(gameFlow.gameBoardArr[0] == 'O' && gameFlow.gameBoardArr[3] =='O' && gameFlow.gameBoardArr[6] == 'O'){
+            title.innerText = player2.congrats();
+        }else if(gameFlow.gameBoardArr[1] == 'O' && gameFlow.gameBoardArr[4] =='O' && gameFlow.gameBoardArr[7] == 'O'){
+            title.innerText = player2.congrats();
+        }else if(gameFlow.gameBoardArr[2] == 'O' && gameFlow.gameBoardArr[5] =='O' && gameFlow.gameBoardArr[8] == 'O'){
+            title.innerText = player2.congrats();
+        }
+        
+        //player 2 diagonal victory
+        if(gameFlow.gameBoardArr[0] == 'O' && gameFlow.gameBoardArr[4] =='O' && gameFlow.gameBoardArr[8] == 'O'){
+            title.innerText = player2.congrats();
+        }else if(gameFlow.gameBoardArr[6] == 'O' && gameFlow.gameBoardArr[4] =='O' && gameFlow.gameBoardArr[2] == 'O'){
+            title.innerText = player2.congrats();
+        }
+        //checks array for empty spaces, creates a tie if none.
+        if(gameFlow.gameBoardArr.indexOf('') > -1){
+            
+        }else{
+            title.innerText = "It's a tie!";
+        }
+   })
+}
 //control the turn variable in gameflow
 function changeTurn(){
     if(gameFlow.turn === 0){
@@ -53,110 +117,143 @@ function updateBoard(space,num){
         gameFlow.gameBoardArr[num] = 'O'
     }
 }
-
-function checkWin(){
-    
+//Used to stop input when there's a winner
+function checkTitle(){
+    if (title.innerText != 'Tic Tac Toe'){
+        return true;
+    }else{
+        return false;
+    }
 }
+
+   
     //event listeners for clicking on the board
 const displayClick = (() =>{
     space1.addEventListener('click', function(){
-        console.log('1');
+       if(checkTitle() === true){
+        return;
+       }else{
         
         if(checkInput(space1) === true){
             updateBoard(space1, 0);
+            checkWin();
             changeTurn();
         }else{
             return;
         }   
+    }
     })
     
     space2.addEventListener('click', function(){
-        console.log('2');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space2) === true){
             updateBoard(space2, 1);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space3.addEventListener('click', function(){
-        console.log('3');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space3) === true){
             updateBoard(space3, 2);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space4.addEventListener('click', function(){
-        console.log('4');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space4) === true){
             updateBoard(space4, 3);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space5.addEventListener('click', function(){
-        console.log('5');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space5) === true){
             updateBoard(space5, 4);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space6.addEventListener('click', function(){
-        console.log('6');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space6) === true){
             updateBoard(space6, 5);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space7.addEventListener('click', function(){
-        console.log('7');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space7) === true){
             updateBoard(space7, 6);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space8.addEventListener('click', function(){
-        console.log('8');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space8) === true){
             updateBoard(space8, 7);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
+    }
     })
     
     space9.addEventListener('click', function(){
-        console.log('9');
-
+        if(checkTitle() === true){
+            return;
+           }else{
         if(checkInput(space9) === true){
             updateBoard(space9, 8);
+            checkWin();
             changeTurn();
         }else{
             return;
         }
-        console.log(gameFlow.gameBoardArr);
+    }
     })
   
 })();
